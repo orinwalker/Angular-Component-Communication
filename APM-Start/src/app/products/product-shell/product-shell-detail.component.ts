06d74fb0b82996a9ef1cc84bbcc1a19e6ead4957
Orin Walker <orin.walker@gmail.com>
2020-03-16 11:06:54 -0400

    wired up alternative view - minor changes

--- APM-Start/src/app/products/product-shell/product-shell-detail.component.ts
+++ APM-Start/src/app/products/product-shell/product-shell-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { IProduct } from '../product';
+import { timer } from 'rxjs/observable/timer';
+
 
 @Component({
     selector: 'pm-product-shell-detail',
@@ -18,6 +20,8 @@ export class ProductShellDetailComponent implements OnInit {
     constructor(private productService: ProductService) { }
 
     ngOnInit() {
+      // console.log(this.productXXXXXXXXXX);
+      //timer(0, 1000).subscribe(t => console.log(this.productXXXXXXXXXX));
     }
 
 }
